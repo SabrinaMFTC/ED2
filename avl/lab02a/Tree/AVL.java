d6297ba2d6da17765527ad04bb80d0e9277e84a0
Sabrina Midori <sabrina.mftc@gmail.com>
2024-04-27 23:47:25 -0300

    update rotateLeft and rotateRight methods

--- avl/lab02a/Tree/AVL.java
+++ avl/lab02a/Tree/AVL.java
@@ -15,31 +15,42 @@ public class AVL extends BST {
 
     public Node rotateLeft(Node node1) {
         Node node2 = node1.getRight();
+        Node node2Left = node2.getLeft();
 
         if (node1.isRoot()) {
             setRoot(node2);
             node2.setParent(null);
+        } else {
+            node1.getParent().setRight(node2);
+            node2.setParent(node1.getParent());
         }
+
         node1.setParent(node2);
-        node1.setRight(node2.getLeft());
-        if (node2.getLeft() != null) node2.getLeft().setParent(node1);
         node2.setLeft(node1);
+
+        node1.setRight(node2Left);
+        if (node2Left != null) node2Left.setParent(node1);
 
         return node2;
     }
 
     public Node rotateRight(Node node3) {
         Node node2 = node3.getLeft();
+        Node node2Right = node2.getRight();
 
         if (node3.isRoot()) {
             setRoot(node2);
             node2.setParent(null);
+        } else {
+            node3.getParent().setLeft(node2);
+            node2.setParent(node3.getParent());
         }
-        node3.setParent(node2);
-        node3.setLeft(node2.getRight());
-        if (node2.getRight() != null) node2.getRight().setParent(node3);
 
+        node3.setParent(node2);
         node2.setRight(node3);
+
+        node3.setLeft(node2Right);
+        if (node2Right != null) node2Right.setParent(node3);
 
         return node2;
     }
